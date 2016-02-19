b3368d0d347ab4e3e242c0cf2eecc8ab3c1fabf3
Geoff Furlong <geoff_furlong@hotmail.com>
2016-02-19 00:40:52 +0000

    GhostScriptPCL v1

---
+++ GhostScriptPCL/GhostScriptPCLWrapper/API/pcl64.cs
@@ -0,0 +1,49 @@
+﻿namespace GhostScriptPCL.API
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Text;
+
+    namespace GhostscriptPCL.API
+    {
+        internal class GhostScriptPCL64
+        {
+            #region Hooks into Ghostscript PDL's 64-bit pcl6-64.dll
+            
+            [DllImport("pcl6-64.dll", EntryPoint = "pl_main")]
+            private static extern int InitAPI(int argc, string[] argv);
+
+            #endregion
+
+            /// <summary>
+            /// GS can only support a single instance, so we need to bottleneck any multi-threaded systems.
+            /// </summary>
+            private static object resourceLock = new object();
+
+            /// <summary>
+            /// Calls the Ghostscript API with a collection of arguments to be passed to it
+            /// </summary>
+            public static void CallAPI(string[] args)
+            {
+                lock (resourceLock)
+                {
+                    try
+                    {
+                        int result = InitAPI(args.Length, args);
+
+                        if (result < 0)
+                        {
+                            throw new ExternalException("Ghostscript conversion error", result);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new ExternalException(string.Concat("GhostScriptPCL error ", ex.Message), ex);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ GhostScriptPCL/README.txt
@@ -0,0 +1,9 @@
+GhostScriptPCL
+2016 Geoff Furlong
+
+
+http://www.columbia.edu/~em36/ghostpcl.html
+pcl6 -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=c:\test.pdf c:\test.pcl
+
+plapi.h...
+pl_main(int argc, char *argv[]);
